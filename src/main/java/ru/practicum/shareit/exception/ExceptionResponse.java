4954ed98a27384fd15cfd77197e1d91d9dafa476
Shjiwa <Shjiwa@yandex.ru>
2023-08-05 19:31:45 +0300

    feat: add milliseconds

--- src/main/java/ru/practicum/shareit/exception/ExceptionResponse.java
+++ src/main/java/ru/practicum/shareit/exception/ExceptionResponse.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 public class ExceptionResponse {
     private final HttpStatus status;
     private final String message;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     private final LocalDateTime time = LocalDateTime.now();
 
     public ExceptionResponse(HttpStatus status, String message) {
