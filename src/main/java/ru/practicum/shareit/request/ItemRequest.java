c1a72809194edec37f61ca9db99daab6c6939ac7
Jasah <Shjiwa@yandex.ru>
2023-08-15 21:19:44 +0300

    feat: add annotations

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,17 +1,34 @@
 package ru.practicum.shareit.request;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.practicum.shareit.user.model.User;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 /**
  * TODO Sprint add-item-requests.
  */
-@Data
+@Entity
+@Table(name = "requests", schema = "public")
+@NoArgsConstructor
+@Getter
+@Setter
 public class ItemRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "requester_id", nullable = false)
     private User requester;
-    private LocalDateTime created;
+
+    @Column
+    private LocalDateTime created = LocalDateTime.now();
 }
