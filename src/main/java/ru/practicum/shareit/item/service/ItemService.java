769fd569bf6da79139c94de565c9d736c4018ab0
Jasah <Shjiwa@yandex.ru>
2023-08-15 21:20:23 +0300

    feat: new services, now they work with repo

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -1,14 +1,17 @@
 package ru.practicum.shareit.item.service;
 
+import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 
 import java.util.Collection;
 
 public interface ItemService {
 
-    ItemDto add(ItemDto itemDto, Long userId);
+    ItemDto addItem(ItemDto itemDto, Long userId);
 
-    ItemDto getItemInfo(Long itemId);
+    CommentDto addComment(CommentDto commentDto, Long itemId, Long userId);
+
+    ItemDto getItemInfo(Long itemId, Long userId);
 
     Collection<ItemDto> getOwnerItems(Long userId);
 
