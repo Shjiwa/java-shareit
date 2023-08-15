6a5828353ffe5da3e560519958819bd471eb2cc5
Shjiwa <Shjiwa@yandex.ru>
2023-08-15 17:45:24 +0300

    code: доработать контроллеры (аннотации/логирование)

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -3,31 +3,42 @@ package ru.practicum.shareit.item.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.service.ItemService;
 
 import javax.validation.Valid;
 import java.util.Collection;
+import static ru.practicum.shareit.constant.Constant.OWNER_ID_HEADER;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/items")
 public class ItemController {
+
     private final ItemService itemService;
-    private static final String OWNER_ID_HEADER = "X-Sharer-User-Id";
 
     @PostMapping
     public ItemDto add(@Valid @RequestBody ItemDto itemDto,
                        @RequestHeader(OWNER_ID_HEADER) Long userId) {
-        log.info("Поступил запрос на добавление вещи от пользователя: {}", userId);
-        return itemService.add(itemDto, userId);
+        log.info("Поступил запрос на добавление вещи {} от пользователя: {}", itemDto, userId);
+        return itemService.addItem(itemDto, userId);
+    }
+
+    @PostMapping("/{itemId}/comment")
+    public CommentDto addComment(@Valid @RequestBody CommentDto commentDto,
+                                 @RequestHeader(OWNER_ID_HEADER) Long userId,
+                                 @PathVariable Long itemId) {
+        log.info("Поступил запрос на добавление отзыва к товару с id: {}, от пользователя с id: {}, comment: {}",
+                itemId, userId, commentDto);
+        return itemService.addComment(commentDto, itemId, userId);
     }
 
     @GetMapping("/{itemId}")
-    public ItemDto getInfo(@PathVariable Long itemId) {
-        log.info("Поступил запрос на получение информации о вещи с id: {}", itemId);
-        return itemService.getItemInfo(itemId);
+    public ItemDto getInfo(@PathVariable Long itemId, @RequestHeader(OWNER_ID_HEADER) Long userId) {
+        log.info("Поступил запрос на получение информации о вещи с id: {} от пользователя с id: {}", itemId, userId);
+        return itemService.getItemInfo(itemId, userId);
     }
 
     @GetMapping
