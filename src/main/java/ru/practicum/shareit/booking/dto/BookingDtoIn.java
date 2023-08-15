5c6f070441a2eb52b722c191079a242601f69d7a
Shjiwa <Shjiwa@yandex.ru>
2023-08-15 17:46:16 +0300

    feat: add Dto's

---
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDtoIn.java
@@ -0,0 +1,30 @@
+package ru.practicum.shareit.booking.dto;
+
+import lombok.Data;
+import ru.practicum.shareit.constant.Status;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+public class BookingDtoIn {
+
+    private Long id;
+
+    @NotNull(message = "Error! Start time can't be null.")
+    @FutureOrPresent(message = "Error! Start time can't be in past.")
+    private LocalDateTime start;
+
+    @NotNull(message = "Error! Start time can't be null.")
+    @FutureOrPresent(message = "Error! Start time can't be in past.")
+    private LocalDateTime end;
+
+    @NotNull(message = "Booking must have an item.")
+    private Long itemId;
+
+    private UserDto booker;
+
+    private Status status;
+}
--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -3,15 +3,18 @@ package ru.practicum.shareit.request.dto;
 import lombok.Data;
 import ru.practicum.shareit.user.model.User;
 
-import java.time.LocalDateTime;
+import javax.validation.constraints.NotNull;
 
 /**
  * TODO Sprint add-item-requests.
  */
 @Data
 public class ItemRequestDto {
+
     private Long id;
+
     private String description;
+
+    @NotNull(message = "Error! Requester can't be null.")
     private User requester;
-    private LocalDateTime created;
 }
