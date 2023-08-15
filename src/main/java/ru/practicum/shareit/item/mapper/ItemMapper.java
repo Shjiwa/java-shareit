f14beaf99d4f40af5ef6cb9711e8478182c373e1
Jasah <Shjiwa@yandex.ru>
2023-08-15 21:17:00 +0300

    refactor: refactor mappers

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,28 +1,49 @@
 package ru.practicum.shareit.item.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
+import ru.practicum.shareit.booking.dto.BookingDtoOwner;
+import ru.practicum.shareit.booking.mapper.BookingMapper;
+import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemDtoOwner;
+import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
-public class ItemMapper {
-    public static ItemDto toItemDto(Item item) {
-        return new ItemDto(
-                item.getId(),
-                item.getName(),
-                item.getDescription(),
-                item.getAvailable()
-        );
-    }
+import java.util.List;
 
-    public static Item toItem(ItemDto itemDto, User owner, ItemRequest itemRequest) {
-        return new Item(
-                itemDto.getId(),
-                itemDto.getName(),
-                itemDto.getDescription(),
-                itemDto.getAvailable(),
-                owner,
-                itemRequest
-        );
+@Mapper(uses = {CommentMapper.class})
+public interface ItemMapper {
+
+    ItemMapper INSTANCE = Mappers.getMapper(ItemMapper.class);
+
+    ItemDto toItemDto(Item item);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "name", source = "itemDto.name")
+    @Mapping(target = "request", ignore = true)
+    @Mapping(target = "owner", source = "owner")
+    Item toItem(ItemDto itemDto, User owner);
+
+    @Mapping(target = "id", source = "itemDto.id")
+    @Mapping(target = "name", source = "itemDto.name")
+    @Mapping(target = "request", ignore = true)
+    @Mapping(target = "owner", source = "owner")
+    Item toItemWithId(ItemDto itemDto, User owner);
+
+    @Mapping(target = "id", source = "item.id")
+    @Mapping(target = "lastBooking", source = "lastBooking", qualifiedByName = "BookingToBookingDtoOwner")
+    @Mapping(target = "nextBooking", source = "nextBooking", qualifiedByName = "BookingToBookingDtoOwner")
+    @Mapping(target = "comments", source = "comments")
+    ItemDtoOwner toItemDtoOwner(Item item, Booking lastBooking, Booking nextBooking, List<Comment> comments);
+
+    List<ItemDto> toItemDtoList(List<Item> items);
+
+    @Named("BookingToBookingDtoOwner")
+    static BookingDtoOwner toBookingDtoOwner(Booking booking) {
+        return BookingMapper.INSTANCE.toBookingDtoOwner(booking);
     }
 }
--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -1,22 +1,24 @@
 package ru.practicum.shareit.user.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.dto.UserDtoShort;
 import ru.practicum.shareit.user.model.User;
 
-public class UserMapper {
-    public static UserDto toUserDto(User user) {
-        return new UserDto(
-                user.getId(),
-                user.getName(),
-                user.getEmail()
-        );
-    }
+@Mapper
+public interface UserMapper {
 
-    public static User toUser(UserDto userDto) {
-        return new User(
-                userDto.getId(),
-                userDto.getName(),
-                userDto.getEmail()
-        );
-    }
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDto toUserDto(User user);
+
+    UserDtoShort toUserDtoShort(User user);
+
+    @Mapping(target = "id", ignore = true)
+    User toUser(UserDto userDto);
+
+    User toUserWithId(UserDto userDto);
+
 }
