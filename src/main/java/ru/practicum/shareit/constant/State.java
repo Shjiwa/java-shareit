743d036f14c3793b72353b9fd0621fdc1ad5638f
Jasah <Shjiwa@yandex.ru>
2023-08-15 21:16:03 +0300

    feat: add constants

---
+++ src/main/java/ru/practicum/shareit/constant/State.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit.constant;
+
+public final class State {
+    private State() {
+    }
+
+    public static final String ALL = "ALL";
+    public static final String CURRENT = "CURRENT";
+    public static final String PAST = "PAST";
+    public static final String FUTURE = "FUTURE";
+    public static final String WAITING = "WAITING";
+    public static final String REJECTED = "REJECTED";
+}
