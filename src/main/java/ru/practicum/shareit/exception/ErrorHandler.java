0849b43a2be463b1ae8f7cbc0be3cb8468cf2083
Jasah <Shjiwa@yandex.ru>
2023-08-15 21:17:21 +0300

    add: new handler

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -30,4 +30,18 @@ public class ErrorHandler {
         log.error(e.getMessage(), e);
         return new ExceptionResponse(HttpStatus.CONFLICT, e.getMessage());
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionResponse handleInternalServerErrorException(final InternalServerErrorException e) {
+        log.error(e.getMessage(), e);
+        return new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ExceptionResponse handleForbiddenException(final ForbiddenException e) {
+        log.error(e.getMessage(), e);
+        return new ExceptionResponse(HttpStatus.FORBIDDEN, e.getMessage());
+    }
 }
