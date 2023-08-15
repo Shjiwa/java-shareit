19b7c8b5c60fb0306d2f77d9f75d58899d275d30
Shjiwa <Shjiwa@yandex.ru>
2023-08-15 17:46:57 +0300

    feat: add constants

---
+++ src/main/java/ru/practicum/shareit/constant/Constant.java
@@ -0,0 +1,9 @@
+package ru.practicum.shareit.constant;
+
+public final class Constant {
+
+    private Constant() {
+    }
+
+    public static final String OWNER_ID_HEADER = "X-Sharer-User-Id";
+}
--- src/main/java/ru/practicum/shareit/constant/Status.java
+++ src/main/java/ru/practicum/shareit/constant/Status.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit.booking.util;
+package ru.practicum.shareit.constant;
 
 public enum Status {
     WAITING,
