e0ce47469031468ad24243f77325cf769b0528c5
Jasah <Shjiwa@yandex.ru>
2023-08-19 20:46:31 +0300

    feat: add new findAllByItemIds method

--- src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Comment;
 
@@ -10,5 +11,10 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByItemId(Long itemId);
+
+    @Query("select c " +
+            "from Comment c " +
+            "where c.item.id in(?1)")
+    List<Comment> findAllByItemIds(List<Long> ids);
 
 }
