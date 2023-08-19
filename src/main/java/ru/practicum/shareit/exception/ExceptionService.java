4fcb278a1a904dffb66b398a2c05cfe0673de46d
Jasah <Shjiwa@yandex.ru>
2023-08-19 20:43:32 +0300

    delete: delete custom ISE

--- src/main/java/ru/practicum/shareit/exception/ExceptionService.java
+++ src/main/java/ru/practicum/shareit/exception/ExceptionService.java
@@ -16,10 +16,6 @@ public class ExceptionService {
         throw new ConflictException(message);
     }
 
-    public void throwInternalServerError(String message) {
-        throw new InternalServerErrorException(message);
-    }
-
     public void throwForbidden(String message) {
         throw new ForbiddenException(message);
     }
