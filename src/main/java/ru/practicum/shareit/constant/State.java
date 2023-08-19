1330fca952c7db63b762635db99130f177982389
Jasah <Shjiwa@yandex.ru>
2023-08-19 20:42:51 +0300

    refactor: State now is enum

--- src/main/java/ru/practicum/shareit/constant/State.java
+++ src/main/java/ru/practicum/shareit/constant/State.java
@@ -1,13 +1,10 @@
 package ru.practicum.shareit.constant;
 
-public final class State {
-    private State() {
-    }
-
-    public static final String ALL = "ALL";
-    public static final String CURRENT = "CURRENT";
-    public static final String PAST = "PAST";
-    public static final String FUTURE = "FUTURE";
-    public static final String WAITING = "WAITING";
-    public static final String REJECTED = "REJECTED";
+public enum State {
+    ALL,
+    CURRENT,
+    PAST,
+    FUTURE,
+    WAITING,
+    REJECTED
 }
