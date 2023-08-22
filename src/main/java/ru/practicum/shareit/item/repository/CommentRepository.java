993d1bb755becc485d36d59e7572e95ab2c58879
Jasah <Shjiwa@yandex.ru>
2023-08-22 23:41:12 +0300

    code: reformat / add new method

--- src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Comment;
 
@@ -12,9 +11,5 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByItemId(Long itemId);
 
-    @Query("select c " +
-            "from Comment c " +
-            "where c.item.id in(?1)")
-    List<Comment> findAllByItemIds(List<Long> ids);
-
+    List<Comment> findAllByItem_IdIn(List<Long> ids);
 }
