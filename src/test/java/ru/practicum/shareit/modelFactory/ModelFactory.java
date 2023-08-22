24ce68f5719d28aa4a29bb9df3feb5e960189036
Jasah <Shjiwa@yandex.ru>
2023-08-22 23:44:37 +0300

    null

--- src/test/java/ru/practicum/shareit/modelFactory/ModelFactory.java
+++ src/test/java/ru/practicum/shareit/modelFactory/ModelFactory.java
@@ -47,6 +47,6 @@ public class ModelFactory {
     }
 
     public void setItemName(Item item) {
-        item.setName(item.getName() + " " + Math.random());
+        item.setName(item.getName() + "  " + Math.random());
     }
 }
