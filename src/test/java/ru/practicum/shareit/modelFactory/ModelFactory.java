584426d1dce48f0a06f6c6aa16a2a58534ab3c90
Jasah <Shjiwa@yandex.ru>
2023-08-24 01:35:48 +0300

    feat: add model factory

--- src/test/java/ru/practicum/shareit/modelFactory/ModelFactory.java
+++ src/test/java/ru/practicum/shareit/modelFactory/ModelFactory.java
@@ -1,7 +1,15 @@
 package ru.practicum.shareit.modelFactory;
 
+import ru.practicum.shareit.booking.dto.BookingDtoIn;
+import ru.practicum.shareit.booking.dto.BookingDtoOut;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.constant.Status;
+import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
 
 public class ModelFactory {
 
@@ -25,28 +33,61 @@ public class ModelFactory {
         }
     }
 
-    public User createUser() {
+    public BookingDtoOut getBookingResponseDto(Long id, LocalDateTime time) {
+        BookingDtoOut dtoOut = new BookingDtoOut();
+        dtoOut.setId(id);
+        dtoOut.setStart(time.plusDays(1));
+        dtoOut.setEnd(time.plusDays(2));
+        return dtoOut;
+    }
+
+    public Booking getBooking(Long id, User booker, Item item) {
+        Booking booking = new Booking();
+        booking.setId(id);
+        booking.setItem(item);
+        booking.setBooker(booker);
+        booking.setStatus(Status.APPROVED);
+        return booking;
+    }
+
+    public BookingDtoIn getBookingDtoIn(LocalDateTime time) {
+        BookingDtoIn bookingDtoIn = new BookingDtoIn();
+        bookingDtoIn.setStart(time.plusDays(1));
+        bookingDtoIn.setEnd(time.plusDays(2));
+        return bookingDtoIn;
+    }
+
+    public UserDto getUserDto() {
+        UserDto userDto = new UserDto();
+        userDto.setName("User");
+        userDto.setEmail("user@mail.ru");
+        return userDto;
+    }
+
+    public User getUser(Long id) {
         User user = new User();
-        user.setName("user");
-        user.setEmail("user@mail.ru");
+        user.setId(id);
+        user.setName("User" + id);
+        user.setEmail("user" + id + "@mail.ru");
         return user;
     }
 
-    public Item createItem(User owner) {
+    public ItemDto getItemDto() {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setName("Item");
+        itemDto.setDescription("ItemDescription");
+        itemDto.setAvailable(true);
+        return itemDto;
+    }
+
+    public Item getItem(Long id, User owner) {
         Item item = new Item();
-        item.setName("film");
-        item.setDescription("film description");
+        item.setId(id);
+        item.setName("Item" + id);
+        item.setDescription("ItemDescription" + id);
         item.setAvailable(true);
         item.setOwner(owner);
+        item.setRequest(null);
         return item;
-    }
-
-    public void setNewUserData(User user) {
-        user.setName(user.getName() + Math.random());
-        user.setEmail(user.getName() + "@mail.ru");
-    }
-
-    public void setItemName(Item item) {
-        item.setName(item.getName() + " " + Math.random());
     }
 }
